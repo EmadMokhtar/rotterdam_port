d6fc2cd92cb3a545d65044189cf391b0ce46dfc9
Emad Mokhtar <emad.m.habib@gmail.com>
2017-02-01 23:24:10 +0300

    Add test for views

--- cargo/tests/test_views.py
+++ cargo/tests/test_views.py
@@ -1,9 +1,41 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from cargo.models import Dock
 
 
 class DockListViewTestCases(TestCase):
-    pass
+
+    def test_view_url(self):
+        self.assertEqual(reverse('dock-list'), '/cargo/docks/')
+
+    def test_empty_dock_list(self):
+        response = self.client.get(reverse('dock-list'))
+        print(response.status_code)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, len(response.context['docks']))
+
+    def test_dock_list(self):
+        for i in range(10):
+            Dock.objects.create(dock_number='DC-{}'.format(i))
+        response = self.client.get(reverse('dock-list'))
+        print(response.status_code)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(10, len(response.context['docks']))
 
 
 class DockDetailViewTestCases(TestCase):
-    pass
+
+    def setUp(self):
+        self.dock = Dock.objects.create(dock_number='DC-01')
+
+    def test_view_url(self):
+        self.assertEqual(reverse('dock-details',
+                                 kwargs={'dock_number': self.dock.dock_number}),
+                         '/cargo/docks/DC-01/')
+
+    def test_dock_details(self):
+        response = self.client.get(reverse('dock-details',
+                                           kwargs={'dock_number': self.dock.dock_number}))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.dock, response.context['dock'])
